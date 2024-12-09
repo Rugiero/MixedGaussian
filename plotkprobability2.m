a7fd6cf411dfb6bd7871fadc174d093b65929c4a
Ilari <iangervuori@gmail.com>
2024-12-09 03:06:34 +0200

    H

--- plotkprobability2.m
+++ plotkprobability2.m
@@ -2,41 +2,36 @@ clear all;
 %close all;
 
 
-S = importdata("kprobssimkappa3exp.mat");
-simres1exp = S.simres1;
-simres2exp = S.simres2;
-simres3exp = S.simres3;
 
+S = importdata("kprobstheory.mat");
+theoryres1 = round(S.theoryres1,2);
+theoryres2 = round(S.theoryres2,2);
+theoryres3 = round(S.theoryres3,2);
+theoryres4 = round(S.theoryres4,2);
 
-S = importdata("kprobstheorykappa3.mat");
-theoryres1 = S.theoryres1;
-theoryres2 = S.theoryres2;
-theoryres3 = S.theoryres3;
-%theoryres4 = S.theoryres4;
-
-S = importdata("kprobssim90kappa3.mat");
+S = importdata("kprobssim90.mat");
 simres190 = S.simres1;
 simres290 = S.simres2;
 simres390 = S.simres3;
-%simres490 = S.simres4;
+simres490 = S.simres4;
 
 %% S = importdata("kprobssim70.mat");
 %% simres170 = S.simres1;
 %% simres270 = S.simres2;
 %% simres370 = S.simres3;
-%simres470 = S.simres4;
+%% simres470 = S.simres4;
 
-S = importdata("kprobssim40kappa3.mat");
+S = importdata("kprobssim40.mat");
 simres140 = S.simres1;
 simres240 = S.simres2;
 simres340 = S.simres3;
-%simres440 = S.simres4;
+simres440 = S.simres4;
 
-S = importdata("kprobssim30kappa3.mat");
-simres130 = S.simres1;
-simres230 = S.simres2;
-simres330 = S.simres3;
-%simres430 = S.simres4;
+%% S = importdata("kprobssim30.mat");
+%% simres130 = S.simres1;
+%% simres230 = S.simres2;
+%% simres330 = S.simres3;
+%% simres430 = S.simres4;
 
 
 
@@ -67,9 +62,6 @@ plot(tau,theoryres2,'-s','color',"#D95319",'linewidth',2)
 plot(tau,theoryres3,'-^','color',"#EDB120",'linewidth',2)
 %plot(tau,theoryres4,'color',"blue",'linewidth',2)
 
-%plot(simtau,simres1exp,'-*','color',"#0072BD",'linewidth',2)
-%plot(simtau,simres2exp,'-s','color',"#D95319",'linewidth',2)
-%plot(simtau,simres3exp,'-^','color',"#EDB120",'linewidth',2)
 
 %plot(simtau,simres190,'--','color',"black",'linewidth',1)
 %plot(simtau,simres290,'--','color',"black",'linewidth',1)
@@ -100,10 +92,12 @@ fill([simtau,fliplr(simtau)],[simres240',fliplr(simres290')],[0.8500 0.3250 0.09
 
 fill([simtau,fliplr(simtau)],[simres340',fliplr(simres390')],[0.9290 0.6940 0.1250],'FaceAlpha',0.3,'EdgeColor','flat');
 
+%% fill([simtau,fliplr(simtau)],[simres440',fliplr(simres490')],[0.9290 0.6940 0.1250],'FaceAlpha',0.3,'EdgeColor','none');
 
 
 
-legend('Theory; k=1','Theory; k=2','Theory; k=3','Simulations for $\epsilon \in [90^{\circ},40^{\circ}]$ and $\tilde{\kappa}=3/b_{\epsilon}$ ','Interpreter','latex',...
+
+legend('Theory; k=1','Theory; k=2','Theory; k=3','Simulations for $\epsilon \in [90^{\circ},40^{\circ}]$, $\tilde{\kappa}=1/b_{\epsilon}$ ','Interpreter','latex',...
     'FontSize',14,BackgroundAlpha=.5)
 
 xlabel('$\tau$(dB)','FontSize',14,'Interpreter','latex')
@@ -115,10 +109,12 @@ axis([[-8,4],[0,1]])
 % Create text
 text('Parent',axes1,'FontSize',14,'Interpreter','latex',...
     'String','$\epsilon=90^{\circ}$',...
-    'Position',[-4.4841628959276 0.479452054794521 0]);
+    'Position',[-0.00452488687782404 0.408219178082192 0]);
 
 % Create text
 text('Parent',axes1,'FontSize',14,'Interpreter','latex',...
     'String','$\epsilon=40^{\circ}$',...
-    'Position',[-2.36651583710407 0.594520547945206 0]);
+    'Position',[1.32579185520363 0.6 0]);
+
+
 latex2axes(figure1,"Times New Roman",14,"normal")
